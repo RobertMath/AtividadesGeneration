0a1b390235834f5f2c5a8736449ee7a30e93e5d5
Robert Matheus <robertmatheusfernandes8@gmail.com>
2024-10-30 17:08:35 -0300

    primeira atividade While atualizada

---
+++ atividade.java.lacos.repeticao/src/lacos/repeticao/While/Atividade1.java
@@ -0,0 +1,10 @@
+package lacos.repeticao.While;
+
+import java.util.Scanner;
+
+public class Atividade1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+	}
+}
--- atividade.java.lacos.repeticao/src/lacos/repeticao/While/Atividade2.java
+++ atividade.java.lacos.repeticao/src/lacos/repeticao/While/Atividade2.java
@@ -6,53 +6,17 @@ public class Atividade2 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		boolean autenticador = false;
-		int pessoasParticipantes = 0, idades = 0, naoBinariosFullStack30 = 0, homensMobile40 = 0, mulheresFront = 0,
-				totalBackend = 0;
+		boolean autenticador = true;
+		int  naoBinariosFullStack30 = 0, homensMobile40 = 0, mulheresFront = 0,totalBackend = 0;	
 		int genero, tipoDesenvolvedor, totalColaboradores = 0, somaIdade = 0;
 		float mediaIdade;
-
-		System.out.println("Digite a idade : ");
-		int idade = scanner.nextInt();
-		somaIdade =+ idade;
-		totalColaboradores++;
-
-		System.out.println("Identidade de gênero : (1-6) ");
-		System.out.println("1 - Mulher Cis ");
-		System.out.println("2 - Homem Cis ");
-		System.out.println("3 - Não Binário");
-		System.out.println("4 - Mulher Trans ");
-		System.out.println("5 - Homem Trans ");
-		System.out.println("6 - Outros ");
-		genero = scanner.nextInt();
-
-		System.out.println("Pessoa Desenvolvedora : (1-4)");
-		System.out.println("1 - Backend");
-		System.out.println("2 - Frontend");
-		System.out.println("3 - Mobile");
-		System.out.println("4 - FullStack");
-		tipoDesenvolvedor = scanner.nextInt();
-
-		if (tipoDesenvolvedor == 1) {
-			totalBackend++;
-		} else if (tipoDesenvolvedor == 2 && (genero == 1 || genero == 4)) {
-			mulheresFront++;
-		} else if (tipoDesenvolvedor == 3 && idade >= 40 && (genero == 2 || genero == 5)) {
-			homensMobile40++;
-		} else if (tipoDesenvolvedor == 4 && idade <= 30 && genero == 3) {
-			naoBinariosFullStack30++;
-		}
-
-		mediaIdade = somaIdade / totalColaboradores;
-		System.out.println("Deseja continuar a leitura de um novo colaborador ? (S/N)");
-		boolean continuar = scanner.next().equalsIgnoreCase("s");
 			
-			while (continuar) {
+			while (autenticador) {
 
 				System.out.println("Digite a idade : ");
-				idade = scanner.nextInt();
+				int idade = scanner.nextInt();
 				totalColaboradores++;
-				somaIdade = +idade;
+				somaIdade =+idade;
 
 				System.out.println("Identidade de gênero : (1-6) ");
 				System.out.println("1 - Mulher Cis ");
@@ -81,11 +45,11 @@ public class Atividade2 {
 				}
 				
 				System.out.println("Deseja continuar a leitura de um novo colaborador ? (S/N)");
-				continuar = scanner.next().equalsIgnoreCase("s");
+				autenticador = scanner.next().equalsIgnoreCase("s");
 				
 			}
 			
-
+			mediaIdade = somaIdade / totalColaboradores;
 		
 		System.out.printf("Número de pessoas desenvolvedoras Backend : %d %n", totalBackend);
 		System.out.printf("Número de Mulheres Cis e Trans desenvolvedoras Frontend : %d %n" , mulheresFront);
@@ -93,6 +57,7 @@ public class Atividade2 {
 		System.out.printf("Número de Não Binários desenvolvedores FullStack menores de 30 anos : %d %n" , naoBinariosFullStack30);
 		System.out.printf("Número de pessoas que responderam à pesquisa : %d %n" , totalColaboradores);
 		System.out.printf("A média de idade das pessoas que responderam à pesquisa : %.2f %n" , mediaIdade);
+		scanner.close();
 		
 	}
 
