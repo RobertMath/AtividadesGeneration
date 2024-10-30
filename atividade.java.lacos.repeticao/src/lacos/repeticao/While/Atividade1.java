9bd7ec2102ccf6643189173ba17fef2d3f2ad1fb
Robert Matheus <robertmatheusfernandes8@gmail.com>
2024-10-30 17:36:41 -0300

    atividade 2 while

--- atividade.java.lacos.repeticao/src/lacos/repeticao/While/Atividade1.java
+++ atividade.java.lacos.repeticao/src/lacos/repeticao/While/Atividade1.java
@@ -5,6 +5,24 @@ import java.util.Scanner;
 public class Atividade1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+		int idade, totalMenores21 = 0, totalMaiores50 = 0;
+		boolean autenticador = true;
+
+		while (autenticador) {
+
+			System.out.println("Digite uma idade: ");
+			idade = scanner.nextInt();
+			
+			if(idade <0 ) {
+				autenticador = false;				
+			}else if(idade <= 20) {
+				totalMenores21++;		
+			}else {
+				totalMaiores50++;
+			}
+		}
+		System.out.printf("Total de pessoas menores de 21 anos : %d %n ", totalMenores21);
+		System.out.printf("Total de pessoas maiores de 50 anos : %d %n ", totalMaiores50);
+		scanner.close();
 	}
 }
